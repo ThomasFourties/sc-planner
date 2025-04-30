f497ee2b0f86f1c448a124fd8ea8aec656f409cb
Thomas Fourties <thomas.fourties@icloud.com>
2025-04-30 15:55:04 +0200

    Try changes front to prod

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="content">yo !</div>
+  <div class="dashboard">
+    <div class="txt-wp">
+      <p class="surtitle">Lundi 12 février</p>
+      <h1 class="h1">Bonjour, Thomas</h1>
+      <p class="soustitle">Aujourd’hui, <span>5 tâches</span> vous sont assignés dans 2 projets différents</p>
+    </div>
+    <div class="dashboard-content">
+      <div class="left"></div>
+      <div class="right"></div>
+    </div>
+  </div>
 </template>
 
 <script setup>
