0ec3fd2e52bcd1ec75b1f2a49eaa63c168383d59
Thomas Fourties <thomas.fourties@icloud.com>
2025-05-20 20:22:27 +0200

    some fixes

--- client/middleware/auth.ts
+++ client/middleware/auth.ts
@@ -1,10 +1,12 @@
 export default defineNuxtRouteMiddleware((to) => {
-  const logged = true;
+  const logged = true; // À remplacer par votre logique d'authentification réelle
 
+  // Si l'utilisateur n'est pas connecté et n'est pas sur la page register
   if (!logged && to.path !== '/register') {
     return navigateTo('/register');
   }
 
+  // Si l'utilisateur est connecté et est sur la page d'accueil ou register
   if (logged && (to.path === '/' || to.path === '/register')) {
     return navigateTo('/dashboard');
   }
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -4,6 +4,12 @@
   </div>
 </template>
 
+<script setup>
+definePageMeta({
+  middleware: ['auth']
+});
+</script>
+
 <style lang="scss" scoped>
 @use '../assets/scss/base/variables' as *;
 @use '../assets/scss/utils/sections' as *;
