8fc44f1352c782cca5f9df77acf0e773cd195062
Thomas Fourties <thomas.fourties@icloud.com>
2025-05-22 16:25:07 +0200

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://sc-planner.thomasfourties.fr'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
