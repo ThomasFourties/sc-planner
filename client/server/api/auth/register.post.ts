969698536c70c31834565c5f089ac3589b410542
Thomas Fourties <thomas.fourties@icloud.com>
2025-06-13 11:31:10 +0200

    feat: implement user authentication API endpoints and add FormLoader component for registration process

---
+++ client/server/api/auth/register.post.ts
@@ -0,0 +1,15 @@
+import { defineEventHandler, readBody } from 'h3'
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig()
+  const body = await readBody(event)
+  
+  const response = await $fetch(`${config.public.API_URL}/auth/register`, {
+    method: 'POST',
+    body,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  return response
+}) 
