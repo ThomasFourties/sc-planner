64e06a45f0267f05892efc7b80d705a1ab670392
Thomas Fourties <thomas.fourties@icloud.com>
2025-07-09 00:37:23 +0200

    refactor: comment out Lenis initialization code for future implementation

--- client/plugins/lenis.ts
+++ client/plugins/lenis.ts
@@ -1,14 +1,13 @@
-import Lenis from 'lenis';
+// import Lenis from 'lenis';
 
 export default defineNuxtPlugin(() => {
-  if (process.client) {
-    if (typeof Lenis === 'undefined') {
-      console.error('Lenis is not defined');
-      return;
-    }
-
-    new Lenis({
-      autoRaf: true,
-    });
-  }
+  // if (process.client) {
+  //   if (typeof Lenis === 'undefined') {
+  //     console.error('Lenis is not defined');
+  //     return;
+  //   }
+  //   new Lenis({
+  //     autoRaf: true,
+  //   });
+  // }
 });
